5bfe8ceb39d0f501e64a9822884eb8b8fb23b54f
iamdeveloper.me <iamdeveloper.me@gmail.com>
2019-06-21 12:41:16 +0530

    Added static url

--- medix/settings/development.py
+++ medix/settings/development.py
@@ -15,6 +15,23 @@ DATABASES = {
     }
 }
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    PROJECT_APPS.child("static"),
+)
+
+MEDIA_ROOT = PROJECT_APPS.child("media")
+MEDIA_URL = '/media/'
+
+DEFAULT_FROM_EMAIL = 'preetamthoughtwin@mailinator.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+
+ROOT_URL = 'http://18.218.141.80'
 # local settings
 try:
     from .local import *
