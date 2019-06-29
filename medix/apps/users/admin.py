00425b08b51fda47d275a4636d1971d4ddfafaf8
iamdeveloper.me <iamdeveloper.me@gmail.com>
2019-06-29 19:52:30 +0530

    fix bug

--- medix/apps/users/admin.py
+++ medix/apps/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Profile,Education
+from .models import Profile,Education,OperatingHours
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Education)
+admin.site.register(OperatingHours)
