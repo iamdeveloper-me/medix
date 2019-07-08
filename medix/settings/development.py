b395ba359946beb5e9b6badc1e7657aff3d2ab56
iamdeveloper.me <iamdeveloper.me@gmail.com>
2019-07-08 15:17:18 +0530

    Change table name

--- medix/settings/development.py
+++ medix/settings/development.py
@@ -7,7 +7,7 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE':   'django.db.backends.postgresql',
-        'NAME':     'medixdb',
+        'NAME':     'medix',
         'USER':     'postgres',
         'PASSWORD': 'psql',
         'HOST':     'localhost',
